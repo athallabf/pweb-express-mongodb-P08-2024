8337baa8ef3b8d350dbea75bc87d9b9ad5ef82b8
adlyatarisa <adlyatarisa@gmail.com>
2024-11-03 13:04:57 +0700

    feat: add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express, { type Express } from 'express';
 import connectDB from './db-connection';
 import productRoutes from './routes/product.route';
+import bookRoutes from './routes/book.route';
+import express, { type Request, type Response, Express } from 'express';
 
 const app: Express = express();
 const port = 3000;
@@ -11,8 +12,13 @@ app.use(express.json());
 // Connect to MongoDB
 connectDB();
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
 // Routes
 app.use('/products', productRoutes);
+app.use('/book', bookRoutes);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
